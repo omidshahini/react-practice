e3f3d517ef90e8827ebbadef51b7ca7b9e401b36
Omid Shahini <moshahini@yahoo.com>
2025-10-27 17:24:09 +0330

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 
+import './App.css'
+
+const messages = [
+  'work out',
+  'take a break',
+  'read a book'
+]
 const App = () => {
   return (
-    <div>omid</div>
+    <div className='w-xl bg-amber-200 mt-4 mx-auto'>
+      <div className='flex flex-row justify-center items-center gap-50 steps'>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </div>
+    </div>
   )
 }
 
