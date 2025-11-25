8b02fdc7c8501dc1c9af120bfc0417375abc6fe7
Omid Shahini <moshahini@yahoo.com>
2025-11-25 03:52:23 +0330

    Date Counter starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,82 @@
 
-const items = [
-  { id : 0,
-    name : 'Americano',
-    ingredients : 'Espresso, Hot Water',
-    price : 10,
-    src : 'images/americano.jpg'
-  },
-
-  { id : 1,
-    name : 'Espresso',
-    ingredients : 'Pure Espresso',
-    price : 8,
-    src : 'images/espresso.jpeg'
-  },
-
-  { id : 2,
-    name : 'Cappuccino',
-    ingredients : 'Espresso, Hot Water, Sugar, Milk',
-    price : 15,
-    src : 'images/cappuccino.jpeg'
-  },
-
-  { id : 3,
-    name : 'Latte',
-    ingredients : 'Espresso, Milk',
-    price : 12,
-    src : 'images/latte.jpeg'
-  },
-
-  { id : 4,
-    name : 'Cortado',
-    ingredients : 'Espresso, Chocolatte',
-    price : 11,
-    src : 'images/cortado.jpeg'
-  },
-
-  { id : 5,
-    name : 'Flat White',
-    ingredients : 'Espresso, Creme',
-    price : 10,
-    src : 'images/flat white.jpeg'
-  },
-
-  { id : 6,
-    name : 'Mocha',
-    ingredients : 'Green Tea, Hot Water',
-    price : 16,
-    src : 'images/mocha.jpeg'
-  },
-
-  { id : 7,
-    name : 'Cold Brew',
-    ingredients : 'Espresso, Ice, Water',
-    price : 9,
-    src : 'images/cold brew.jpeg'
-  },
-]
-
-
-
-const Menu = () => {
+///// SAMPLE FLASH CARD /////
+{/*
+function UserCard ( {name, age, email, onClick} ) {
   return (
-    <div>
-    {items.map((item, id) => (
-      <div key={id} className="flex flex-col">
-        <img src={item.src} alt={item.name} style={{ width: '25%' }}/>
-        <h2>{item.name}</h2>
-        <p>{item.ingredients}</p>
-        <p><strong>{item.price}$</strong></p>
-      </div>
-    ))}
+    <div onClick={onClick}
+    style={{
+        border: '1px solid #ccc',
+        padding: '10px',
+        marginBottom: '10px',
+        cursor: 'pointer',
+        borderRadius: '5px',
+        backgroundColor: '#f9f9f9',
+      }}>
+      <h2>{name}</h2>
+      <p>{age}</p>
+      <p>{email}</p>
     </div>
   )
 }
-export default Menu;
+
+
+function App () {
+  const users = [
+    { id: 1, name: 'Alice', age: 28, email: 'alice@example.com' },
+    { id: 2, name: 'Bob', age: 34, email: 'bob@example.com' },
+    { id: 3, name: 'Charlie', age: 25, email: 'charlie@example.com' },
+  ];
+
+  const handleUserClick = (id) => {
+    console.log(`user id is : ${id}`)
+  }
+
+  return (
+    <div>
+      {users.map(user => 
+        <UserCard
+        key={user.id} 
+        name={user.name}
+        age={user.age}
+        email={user.email}
+        onClick={() => handleUserClick(user.id)}
+        />
+
+      )}
+    </div>
+  )
+}
+
+export default App*/}
+
+
+//////// DATE COUNTER /////
+
+import { useState } from 'react';
+
+const DateCounter = () => {
+
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1); 
+
+  const date = new Date("november 25 2025");
+  date.setDate(date.getDate() + count);
+
+  () => setStep((c) => c - 1)
+ 
+  return (
+
+    <div className='mt-10 mx-40'>
+      <button onClick={() => setStep((s) => s - 1)}>-</button>
+      <span>step is : {step}</span>
+      <button onClick={() => setStep((s) => s + 1)}>+</button>
+      <br /><br />
+      <button onClick={() => setCount((c) => c - step)}>-</button>
+      <span>count is : {count}</span>
+      <button onClick={() => setCount((c) => c + step)}>+</button>
+      <p>{date.toString()}</p>
+    </div>
+  )
+}
+
+export default DateCounter
